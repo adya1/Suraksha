0af4ba3f157fc7f9dd02b4f429711a031b983bdc
adya1 <adya.jha98@gmail.com>
2018-01-28 14:42:05 +0530

    Safety TabLayoutData added

---
+++ app/src/main/java/com/apkglobal/suraksha/Holder.java
@@ -0,0 +1,35 @@
+package com.apkglobal.suraksha;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by dell on 1/28/2018.
+ */
+
+class Holder extends RecyclerView.ViewHolder {
+    TextView choice;
+
+    Context context;
+
+    String type,demand_name;
+
+    public Holder(final View itemView)  {
+        super(itemView);
+        context=itemView.getContext();
+
+
+        choice=(TextView) itemView.findViewById(R.id.text_of_choice);
+
+
+
+    }
+
+    public void bindData(Model skillModel)
+    {
+        choice.setText(skillModel.name_of_choice);
+
+    }
+}
